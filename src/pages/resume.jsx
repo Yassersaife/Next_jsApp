e8333eb8f5f1b4fc6621ab611bb8ed1ab8aa7c7a
yassersaife <s11840085@stu,najah,edu>
2023-09-03 16:54:13 +0300

    update my cv

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import { baseUrl } from '../seo.config'
-import cv from '@/../public/mycv.pdf'
+import cv from '../images/pdf/cv.pdf';
 
 
   const handleDownload = () => {
